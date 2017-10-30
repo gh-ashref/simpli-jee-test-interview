7398d7fa31e0ccc4dacc95b19deaf9313f93a2c6
gh-ashref <achref.ghribi@gmail.com>
2017-10-30 21:10:53 +0100

    Test DataTranformer

--- src/test/java/fr/simplifia/transform/SmpDataTransformerTest.java
+++ src/test/java/fr/simplifia/transform/SmpDataTransformerTest.java
@@ -1,8 +1,12 @@
 package fr.simplifia.transform;
 
 import fr.simplifia.input.validator.SmpInputValidator;
+import fr.simplifia.transform.impl.SmpDefaultDataTransformer;
+import fr.simplifia.transform.impl.SmpEnglishDataTransformer;
 import org.mockito.Mockito;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 
 public class SmpDataTransformerTest {
@@ -17,23 +21,28 @@ public class SmpDataTransformerTest {
 
     @Test
     public void testTransformOk() throws Exception {
+        SmpDataTransformer transformer = new SmpEnglishDataTransformer(validator);
+        assertEquals(transformer.transform("Ghribi"), "Ghribi : Welcome to Simplifia!");
 
     }
 
     @Test
     public void testTransformNotOk() throws Exception {
-
+        SmpDataTransformer transformer = new SmpEnglishDataTransformer(validator);
+        transformer.transform("Héllo");
     }
 
 
     @Test
     public void testTransformEmpty() throws Exception {
-
+        SmpDataTransformer transformer = new SmpDefaultDataTransformer(validator);
+        assertEquals(transformer.transform(""), " : Welcome to Simplifia!");
     }
 
     @Test
     public void testTransformNull() throws Exception {
-
+        SmpDataTransformer transformer = new SmpDefaultDataTransformer(validator);
+        transformer.transform(null);
     }
 
 
